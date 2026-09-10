874f3efcc0187d89c15c2d92ebd31e4b9bbfabb3
Mirajes <myrrrclone@gmail.com>
2026-09-10 18:46:54 +0300

    player to MonoBehaviour

--- Assets/Scripts/MonoBeh/MBh_LightsBlink.cs
+++ Assets/Scripts/MonoBeh/MBh_LightsBlink.cs
@@ -5,6 +5,7 @@ public class MBh_LightsBlink : MonoBehaviour
     [Header("CORE")]
     [SerializeField] private Light _pointLight;
     [SerializeField] private AnimationCurve _curve;
+    [SerializeField] private Material _material;
 
     [Header("Light Settings")]
     [SerializeField] private Color _lightColor = Color.yellow;
@@ -24,53 +25,10 @@ public class MBh_LightsBlink : MonoBehaviour
     private void BlinkLight()
     {
         _pointLight.intensity = _curve.Evaluate(Time.time * _lightSpeed) * _maxLightIntensity;
+        _material.color = new Color(
+            _curve.Evaluate(Time.time * _lightSpeed),
+            _curve.Evaluate(Time.time * _lightSpeed),
+            0
+        );
     }
 }
-
-//[RequireComponent(typeof(CharacterController))]
-//public class MBh_Player : MonoBehaviour
-//{
-//    [SerializeField] private CharacterController _controller;
-//    [SerializeField] private Camera _camera;
-
-//    [SerializeField] private float _moveSpeed = 3f;
-//    [SerializeField] private float _jumpPower = 5f;
-//    private bool _isJumping;
-//    [SerializeField] private float _gravityMultiplier = 1f;
-//    private Vector3 
-//    public bool IsGrounded => _controller.isGrounded;
-
-//    [SerializeField] private float _health = 100f;
-
-//    private void Awake()
-//    {
-        
-//    }
-
-//    private void SetGravity()
-//    {
-//        if (IsGrounded && )
-//    }
-//    private void HandleMove()
-//    {
-
-//    }
-//    private void HandleRotate()
-//    {
-
-//    }
-
-//    public void OnMoveInput(InputAction.CallbackContext context)
-//    {
-
-//    }
-//    public void OnRotateInput(InputAction.CallbackContext context)
-//    {
-
-//    }
-
-//    public void OnJumpInput(InputAction.CallbackContext context)
-//    {
-
-//    }
-//}
